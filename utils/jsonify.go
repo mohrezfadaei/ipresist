a902d55ede22de1115a773b5f5a5d7ae2e1145d1
Mohammad Reza Fadaei <mohrezfadaei@gmail.com>
2024-08-12 20:49:56 +0330

    feat(utils): update Jsonify function

--- utils/jsonify.go
+++ utils/jsonify.go
@@ -8,6 +8,13 @@ import (
 var STATUS_MESSAGES = map[int]string{
 	100: "OK",
 	101: "Resource is not implemented",
+	102: "Invalid UUID format",
+	103: "Resource not found",
+	104: "Bad request",
+	105: "Failed to create resource",
+	106: "Failed to update resource",
+	107: "Failed to delete resource",
+	108: "Invalid data provided",
 }
 
 func Jsonify(state interface{}, metadata interface{}, status int, code int, headers map[string]string) fiber.Map {
